7013485ec7bc59e33ce402f9f93e4cc2ff4a19f0
sohnryang <ryang030528@gmail.com>
2017-04-24 21:21:10 +0900

    solved problem 1000

---
+++ problem_1000/solution_python.py
@@ -0,0 +1,11 @@
+"""
+Problem 1000
+https://www.acmicpc.net/problem/1000
+"""
+
+def solve(input_1, input_2):
+    """solves problem"""
+    return input_1 + input_2
+
+IN1, IN2 = [int(x) for x in input().split()]
+print(solve(IN1, IN2))
