c787b6edc10cf9eaf756d240cea7bfd60610a2b7
sohnryang <ryang030528@gmail.com>
2017-04-28 22:09:54 +0900

    solved problem 10998

---
+++ problem_10998/solution_python.py
@@ -0,0 +1,13 @@
+"""
+Problem 10998
+https://www.acmicpc.net/problem/10998
+"""
+
+
+def solve(input_1, input_2):
+    """solves problem"""
+    return input_1 * input_2
+
+
+IN1, IN2 = [int(x) for x in input().split()]
+print(solve(IN1, IN2))
