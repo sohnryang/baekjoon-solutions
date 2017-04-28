0879dabd89dcd08ca3ff481a8a3d50990133f5a0
sohnryang <ryang030528@gmail.com>
2017-04-28 21:58:29 +0900

    changed style

--- problem_2903/solution_python.py
+++ problem_2903/solution_python.py
@@ -3,10 +3,12 @@ Problem 2903
 https://www.acmicpc.net/problem/2903
 """
 
+
 def count_number_of_dots(iterations):
     """Solves problem (given number of iterations)"""
     number_of_dots = (2 ** iterations + 1) ** 2
     return number_of_dots
+
 
 i = int(input())
 print(count_number_of_dots(i))
