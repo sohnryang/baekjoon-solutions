58cf0a81b091f644e83270a38300e918f9941334
sohnryang <ryang030528@gmail.com>
2017-04-29 12:23:13 +0900

    solved problem 1008

---
+++ problem_1008/solution_python.py
@@ -0,0 +1,13 @@
+"""
+Problem 1008
+https://www.acmicpc.net/problem/1008
+"""
+
+
+def solve(input_1, input_2):
+    """solves problem"""
+    return input_1 / input_2
+
+
+IN1, IN2 = [int(x) for x in input().split()]
+print(solve(IN1, IN2))
