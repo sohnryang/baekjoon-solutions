e8c9c7f97689237b24fe7112cf83ab0e52d9ea3f
sohnryang <ryang030528@gmail.com>
2017-04-29 13:16:02 +0900

    solved problem 2558

---
+++ problem_2558/solution_python.py
@@ -0,0 +1,15 @@
+"""
+Problem 2558
+https://www.acmicpc.net/problem/2558
+"""
+
+
+def main():
+    """main function"""
+    in1 = int(input())
+    in2 = int(input())
+    print(in1 + in2)
+
+
+if __name__ == "__main__":
+    main()
