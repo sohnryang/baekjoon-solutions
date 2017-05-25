efe4e79de1e376f7e32c32fa2bb5683662be53f2
sohnryang <ryang030528@gmail.com>
2017-05-25 23:10:08 +0900

    solved problem 2440

---
+++ problem_2440/solution_python.py
@@ -0,0 +1,15 @@
+"""
+Problem 2440
+https://www.acmicpc.net/problem/2440
+"""
+
+
+def main():
+    """main function"""
+    input_n = int(input())
+    for i in range(input_n, 0, -1):
+        print('*' * i)
+
+
+if __name__ == "__main__":
+    main()
