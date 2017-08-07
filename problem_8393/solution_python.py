1dc2b15b4b15253a8c61dfab7e0d2f6187c3cf6e
sohnryang <ryang030528@gmail.com>
2017-08-07 21:40:38 +0900

    solved problem 8393

---
+++ problem_8393/solution_python.py
@@ -0,0 +1,14 @@
+"""
+Problem 8393
+https://www.acmicpc.net/problem/8393
+"""
+
+
+def main():
+    """main function"""
+    input_n = int(input())
+    print(sum(list(range(1, input_n + 1))))
+
+
+if __name__ == "__main__":
+    main()
