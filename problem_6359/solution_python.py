6e877080dcec4740b58ec5372a0785403c1a662d
sohnryang <loop.infinitely@gmail.com>
2018-12-20 03:31:39 +0000

    solved problem 6359

---
+++ problem_6359/solution_python.py
@@ -0,0 +1,11 @@
+"""
+Baekjoon Online Judge #6359
+https://www.acmicpc.net/problem/6359
+"""
+
+import math
+
+in_t = int(input())
+for i in range(in_t):
+    in_n = int(input())
+    print(int(math.sqrt(in_n)))
