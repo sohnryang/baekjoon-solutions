969d28a44b5222d8fe768b5a0924d6b95beea781
손량 <loop.infinitely@gmail.com>
2019-01-19 00:08:04 +0900

    solved problem 11365

---
+++ problem_11365/solution_python.py
@@ -0,0 +1,11 @@
+"""
+Baekjoon Online Judge #11365
+https://www.acmicpc.net/problem/11365
+"""
+
+while True:
+    in_str = input()
+    if in_str == 'END':
+        break
+    else:
+        print(in_str[::-1])
