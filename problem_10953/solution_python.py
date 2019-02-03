3e5a78901439ccbd140ce83c152f1a0d6368edd5
손량 <loop.infinitely@gmail.com>
2019-02-03 10:50:55 +0900

    solved problem 10953

---
+++ problem_10953/solution_python.py
@@ -0,0 +1,9 @@
+"""
+Baekjoon Online Judge #10953
+https://www.acmicpc.net/problem/10953
+"""
+
+in_t = int(input())
+for i in range(in_t):
+    [in_a, in_b] = map(int, input().split(','))
+    print(in_a + in_b)
