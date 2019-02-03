0e0ccb671574b1ac29bb9609b8769a28a11ec8be
손량 <loop.infinitely@gmail.com>
2019-02-03 10:46:24 +0900

    solved problem 10951

---
+++ problem_10951/solution_c++.cc
@@ -0,0 +1,15 @@
+/*
+ * Baekjoon Online Judge #10951
+ * https://www.acmicpc.net/problem/10951
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int in_a, in_b;
+    while (scanf("%d %d", &in_a, &in_b) != -1)
+        printf("%d\n", in_a + in_b);
+    return 0;
+}
