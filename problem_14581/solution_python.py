3fb4bd8de60aa7a01e43964bc5ba3d347d6cf223
손량 <loop.infinitely@gmail.com>
2019-02-15 22:25:34 +0900

    solved problem 14581

---
+++ problem_14581/solution_python.py
@@ -0,0 +1,9 @@
+"""
+Baekjoon Online Judge #14581
+https://www.acmicpc.net/problem/14581
+"""
+
+in_nick = input()
+print(':fan::fan::fan:')
+print(':fan::%s::fan:' % (in_nick))
+print(':fan::fan::fan:')
