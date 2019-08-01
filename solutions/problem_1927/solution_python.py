bf4af287581ae0d1e61750d810b57cb1b6da31cc
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-01 22:47:03 +0900

    Solve problem 1927

---
+++ solutions/problem_1927/solution_python.py
@@ -0,0 +1,19 @@
+"""
+Baekjoon Online Judge #1927
+https://www.acmicpc.net/problem/1927
+"""
+
+from heapq import heappush, heappop
+from sys import stdin
+
+N = int(stdin.readline().strip())
+pq = []
+for _ in range(N):
+    X = int(stdin.readline().strip())
+    if X == 0:
+        if not pq:
+            print(0)
+        else:
+            print(heappop(pq))
+    else:
+        heappush(pq, X)
