f45df449f40f05751253c3010e74e88b15c5c10a
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-06 20:14:23 +0900

    Solve problem 15639

---
+++ solutions/problem_15639/solution_python.py
@@ -0,0 +1,11 @@
+"""
+Baekjoon Online Judge #15639
+https://www.acmicpc.net/problem/15639
+"""
+
+S = input()
+if S in ['Give you up', 'Let you down', 'Run around and desert you',
+         'Make you cry', 'Say goodbye', 'Tell a lie and hurt you']:
+    print('NO')
+else:
+    print('YES')
