eb8148a14e244325b492b6844129cd2a7b7f707e
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-18 23:59:25 +0900

    Partially solve problem 11866

---
+++ solutions/problem_11866/solution_python.py
@@ -0,0 +1,23 @@
+"""
+Baekjoon Online Judge #11866
+https://www.acmicpc.net/problem/11866
+"""
+
+N, K = map(int, input().split())
+people = list(range(1, N + 1))
+
+print('<', end='')
+first = True
+kill = K
+while len(people) > 1:
+    if first:
+        first = False
+    else:
+        print(', ', end='')
+    print(people[kill - 1], end='')
+    del people[kill - 1]
+    kill -= 1
+    kill = (kill + K) % len(people)
+    if kill == 0:
+        kill = len(people)
+print(', %d>' % people[0])
