bd5213eb9b0603f3339405e05faa50a895831192
Ryang Sohn <loop.infinitely@gmail.com>
2020-04-15 16:44:18 +0900

    Add stub for problem 3763

---
+++ solutions/problem_3763/solution_c++.cc
@@ -0,0 +1,56 @@
+/*
+ * Baekjoon Online Judge #3763
+ * https://www.acmicpc.net/problem/3763
+ */
+
+#include <ios>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string raw_sudoku[16];
+int sudoku[16][16];
+
+struct column_object {
+  column_object* left;
+  column_object* right;
+  column_object* up;
+  column_object* down;
+  column_object* column_top;
+};
+
+const int SUDOKU_SIZE = 16, MATRIX_COLS = 16 * 16 * 16;
+column_object master;
+column_object matrix[MATRIX_COLS];
+
+void process_board() {
+  for (int i = 0; i < SUDOKU_SIZE; ++i) {
+    for (int j = 0; j < SUDOKU_SIZE; ++j) {
+      if (raw_sudoku[i][j] == '-')
+        sudoku[i][j] = 0;
+      else
+        sudoku[i][j] = raw_sudoku[i][j] - 'A' + 1;
+    }
+  }
+}
+
+void construct_matrix() {
+  master.right = &matrix[0];
+  matrix[0].left = &master;
+  master.left = &matrix[SUDOKU_SIZE - 1];
+  matrix[SUDOKU_SIZE - 1].right = &master;
+  for (int i = 1; i < MATRIX_COLS; ++i) {
+    matrix[i].left = &matrix[i - 1];
+    matrix[i - 1].right = &matrix[i];
+  }
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  for (int i = 0; i < SUDOKU_SIZE; ++i) cin >> raw_sudoku[i];
+  process_board();
+  construct_matrix();
+  return 0;
+}
