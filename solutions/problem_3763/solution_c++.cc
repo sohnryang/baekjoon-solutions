506e98aa97b3a5e702f7d0ecdcc97145daf0b36e
Ryang Sohn <loop.infinitely@gmail.com>
2020-04-19 16:42:10 +0900

    Add header construction

--- solutions/problem_3763/solution_c++.cc
+++ solutions/problem_3763/solution_c++.cc
@@ -5,6 +5,9 @@
 
 #include <ios>
 #include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
 
 using namespace std;
 
@@ -15,16 +18,53 @@ struct column_object {
   column_object* down;
   column_object* column_top;
   int size;
-  string name;
+  tuple<int, int, int> name;
 };
 
 const int SIZE = 16, BOX_SIZE = 4;
 string raw_sudoku[SIZE];
 int sudoku[SIZE][SIZE];
+column_object* master;
+vector<column_object*> headers;
+
+void process_sudoku() {
+  for (int i = 0; i < SIZE; ++i)
+    for (int j = 0; j < SIZE; ++j) {
+      char c = raw_sudoku[i][j];
+      sudoku[i][j] = c == '-' ? 0 : c - 'A' + 1;
+    }
+}
+
+void construct_headers() {
+  master = new column_object();
+  master->up = master;
+  master->down = master;
+
+  column_object* prev_col = master;
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < SIZE; ++j) {
+      for (int k = 0; k < SIZE; ++k) {
+        column_object* col = new column_object();
+        col->left = prev_col;
+        prev_col->right = col;
+        col->up = col;
+        col->down = col;
+        col->name = {i, j, k};
+        headers.push_back(col);
+        prev_col = col;
+      }
+    }
+  }
+  master->left = prev_col;
+}
+
+void construct_matrix() { construct_headers(); }
 
 int main() {
   cin.tie(nullptr);
   ios_base::sync_with_stdio(false);
   for (int i = 0; i < SIZE; ++i) cin >> raw_sudoku[i];
+  process_sudoku();
+  construct_matrix();
   return 0;
 }
