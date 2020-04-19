08565ffaeaf1cec3a009164e69ab38e22495dd9d
Ryang Sohn <loop.infinitely@gmail.com>
2020-04-19 22:02:52 +0900

    This somewhat works

--- solutions/problem_3763/solution_c++.cc
+++ solutions/problem_3763/solution_c++.cc
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -31,7 +32,7 @@ void process_sudoku() {
   for (int i = 0; i < SIZE; ++i)
     for (int j = 0; j < SIZE; ++j) {
       char c = raw_sudoku[i][j];
-      sudoku[i][j] = c == '-' ? 0 : c - 'A' + 1;
+      sudoku[i][j] = c == '-' ? -1 : c - 'A';
     }
 }
 
@@ -66,12 +67,12 @@ void construct_rows() {
   for (int i = 0; i < SIZE; ++i) {
     for (int j = 0; j < SIZE; ++j) {
       int val = sudoku[i][j];
-      int a = val != 0 ? val : 0, b = val != 0 ? val + 1 : SIZE;
+      int a = val != -1 ? val : 0, b = val != -1 ? val + 1 : SIZE;
       for (int k = a; k < b; ++k) {
         int grid_col = i * SIZE + j;
         column_object* last = headers[grid_col]->up;
         column_object* grid_row = new column_object();
-        grid_row->name = {0, grid_col, k};
+        grid_row->name = {i, j, k};
         grid_row->up = last;
         last->down = grid_row;
         grid_row->down = headers[grid_col];
@@ -82,7 +83,7 @@ void construct_rows() {
         int row_col = SIZE * SIZE + i * SIZE + k;
         last = headers[row_col]->up;
         column_object* row_row = new column_object();
-        row_row->name = {1, i, k};
+        row_row->name = {i, j, k};
         row_row->up = last;
         last->down = row_row;
         row_row->down = headers[row_col];
@@ -95,7 +96,7 @@ void construct_rows() {
         int col_col = SIZE * SIZE * 2 + j * SIZE + k;
         last = headers[col_col]->up;
         column_object* col_row = new column_object();
-        col_row->name = {2, j, k};
+        col_row->name = {i, j, k};
         col_row->up = last;
         last->down = col_row;
         col_row->down = headers[col_col];
@@ -109,7 +110,7 @@ void construct_rows() {
         int box_col = SIZE * SIZE * 3 + box * SIZE + k;
         last = headers[box_col]->up;
         column_object* box_row = new column_object();
-        box_row->name = {3, box, k};
+        box_row->name = {i, j, k};
         box_row->up = last;
         last->down = box_row;
         box_row->down = headers[box_col];
@@ -130,11 +131,77 @@ void construct_matrix() {
   construct_rows();
 }
 
+unordered_map<int, column_object*> current_solution;
+char sudoku_solution[SIZE][SIZE];
+bool finalized = false;
+
+void finalize() {
+  if (finalized) return;
+  finalized = true;
+  for (auto elem : current_solution) {
+    int cell = get<2>(elem.second->name);
+    if (cell == -1) continue;
+    char sol = cell + 'A';
+    int y = get<0>(elem.second->name);
+    int x = get<1>(elem.second->name);
+    cout << y << ' ' << x << ' ' << sol << ' ' << '\n';
+    sudoku_solution[y][x] = sol;
+  }
+  for (int i = 0; i < SIZE; ++i) cout << sudoku_solution[i] << '\n';
+}
+
+void cover(column_object* col) {
+  col->right->left = col->left;
+  col->left->right = col->right;
+  for (column_object* i = col->down; i != col; i = i->down) {
+    for (column_object* j = i->right; j != i; j = j->right) {
+      j->down->up = j->up;
+      j->up->down = j->down;
+      j->column_top->size--;
+    }
+  }
+}
+
+void uncover(column_object* col) {
+  for (column_object* i = col->up; i != col; i = i->up) {
+    for (column_object* j = i->left; j != i; j = j->left) {
+      j->column_top->size++;
+      j->down->up = j;
+      j->up->down = j;
+    }
+  }
+  col->right->left = col;
+  col->left->right = col;
+}
+
+void dlx(int k) {
+  if (master->right == master) {
+    finalize();
+    return;
+  }
+  column_object* c = master->right;
+  for (column_object* j = master->right; j != master; j = j->right)
+    if (j->size < c->size) c = j;
+  cover(c);
+  for (column_object* r = c->down; r != c; r = r->down) {
+    current_solution[k] = r;
+    for (column_object* j = r->right; j != r; j = j->right)
+      cover(j->column_top);
+    dlx(k + 1);
+    r = current_solution[k];
+    c = r->column_top;
+    for (column_object* j = r->left; j != r; j = j->left)
+      uncover(j->column_top);
+  }
+  uncover(c);
+}
+
 int main() {
   cin.tie(nullptr);
   ios_base::sync_with_stdio(false);
   for (int i = 0; i < SIZE; ++i) cin >> raw_sudoku[i];
   process_sudoku();
   construct_matrix();
+  dlx(0);
   return 0;
 }
