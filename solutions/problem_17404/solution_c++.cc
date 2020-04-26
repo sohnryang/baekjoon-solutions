6d96bff0f093f1d89b84cdf6b15b025785a2e913
Ryang Sohn <loop.infinitely@gmail.com>
2020-04-26 15:11:58 +0900

    Solve problem 17404
    Using bottom-up dp

--- solutions/problem_17404/solution_c++.cc
+++ solutions/problem_17404/solution_c++.cc
@@ -14,22 +14,6 @@ using namespace std;
 const int MAX_N = 1000;
 int N, C[MAX_N + 1][3], dp[3][3][MAX_N + 1];
 
-int color_house(int a, int b, int n) {
-  if (n == N) return 0;
-  int &ret = dp[a][b][n];
-  if (ret != -1) return ret;
-  for (int i = 0; i < 3; ++i) {
-    if (n == N - 2 && i == a) continue;
-    if (i == b) continue;
-    if (ret == -1)
-      ret = color_house(a, i, n + 1);
-    else
-      ret = min(ret, color_house(a, i, n + 1));
-  }
-  ret += C[n][b];
-  return ret;
-}
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -37,9 +21,24 @@ int main() {
   cin >> N;
   for (int i = 0; i < N; ++i) cin >> C[i][0] >> C[i][1] >> C[i][2];
 
-  memset(dp, -1, sizeof(dp));
-  cout << min(color_house(0, 0, 0),
-              min(color_house(1, 1, 0), color_house(2, 2, 0)))
-       << '\n';
+  for (int a = 0; a < 3; ++a) {
+    for (int n = N - 1; n >= 0; --n) {
+      if (n == 0) {
+        int c1 = (a + 1) % 3, c2 = (a + 2) % 3;
+        dp[a][a][0] = min(dp[a][c1][1], dp[a][c2][1]) + C[0][a];
+        continue;
+      }
+      for (int b = 0; b < 3; ++b) {
+        if (n == N - 1 && b == a) {
+          dp[a][b][n] = numeric_limits<int>::max();
+          continue;
+        }
+        int c1 = (b + 1) % 3, c2 = (b + 2) % 3;
+        dp[a][b][n] = min(dp[a][c1][n + 1], dp[a][c2][n + 1]) + C[n][b];
+      }
+    }
+  }
+
+  cout << min(dp[0][0][0], min(dp[1][1][0], dp[2][2][0])) << '\n';
   return 0;
 }
