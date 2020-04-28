ed11c16bc179ddfa98d780ab39767335ed661662
Ryang Sohn <loop.infinitely@gmail.com>
2020-04-28 23:17:01 +0900

    Solve problem 14287

--- solutions/problem_14287/solution_c++.cc
+++ solutions/problem_14287/solution_c++.cc
@@ -6,7 +6,6 @@
 #include <algorithm>
 #include <ios>
 #include <iostream>
-#include <numeric>
 #include <vector>
 
 using namespace std;
@@ -15,66 +14,35 @@ int N, M, current_idx = -1;
 vector<vector<int>> graph;
 vector<int> range_lo, range_hi;
 
-class RangeSum {
+class FenwickTree {
  private:
-  vector<long long> tree;
-  vector<long long> lazy;
-  int len;
-
-  void update_lazy(int node, int lo, int hi) {
-    if (lazy[node] == 0) return;
-    tree[node] += (hi - lo + 1) * lazy[node];
-    if (lo != hi) {
-      lazy[node * 2] += lazy[node];
-      lazy[node * 2 + 1] += lazy[node];
-    }
-    lazy[node] = 0;
-  }
-
-  long long query(int lo, int hi, int node, int node_lo, int node_hi) {
-    update_lazy(node, node_lo, node_hi);
-    if (hi < node_lo || node_hi < lo) return 0;
-    if (lo <= node_lo && node_hi <= hi) return tree[node];
-    int mid = (node_lo + node_hi) / 2;
-    return query(lo, hi, node * 2, node_lo, mid) +
-           query(lo, hi, node * 2 + 1, mid + 1, node_hi);
-  }
-
-  void update_range(int lo, int hi, long long delta, int node, int node_lo,
-                    int node_hi) {
-    update_lazy(node, node_lo, node_hi);
-    if (hi < node_lo || lo > node_hi) return;
-    if (lo <= node_lo && node_hi <= hi) {
-      tree[node] += (node_hi - node_lo + 1) * delta;
-      if (node_lo != node_hi) {
-        lazy[node * 2] += delta;
-        lazy[node * 2 + 1] += delta;
-      }
-      return;
-    }
-    int mid = (node_lo + node_hi) / 2;
-    update_range(lo, hi, delta, node * 2, node_lo, mid);
-    update_range(lo, hi, delta, node * 2 + 1, mid + 1, node_hi);
-    tree[node] = tree[node * 2] + tree[node * 2 + 1];
-  }
+  vector<int> tree;
 
  public:
-  long long query(int lo, int hi) { return query(lo, hi, 1, 0, len - 1); }
+  FenwickTree(int n) : tree(n + 1) {}
 
-  void update_range(int lo, int hi, long long delta) {
-    update_range(lo, hi, delta, 1, 0, len - 1);
+  int range_sum(int pos) {
+    ++pos;
+    int ret = 0;
+    while (pos > 0) {
+      ret += tree[pos];
+      pos &= (pos - 1);
+    }
+    return ret;
   }
 
-  RangeSum(int _len) {
-    len = _len;
-    tree.resize(len * 4);
-    lazy.resize(len * 4);
+  void update(int pos, int val) {
+    ++pos;
+    while (pos < tree.size()) {
+      tree[pos] += val;
+      pos += (pos & -pos);
+    }
   }
 };
 
 void dfs(int here) {
   range_lo[here] = ++current_idx;
-  for (int there: graph[here]) {
+  for (int there : graph[here]) {
     if (range_lo[there] != -1) continue;
     dfs(there);
   }
@@ -96,7 +64,7 @@ int main() {
     graph[b - 1].push_back(i);
   }
   dfs(0);
-  RangeSum rsum(N);
+  FenwickTree rsum(N);
   for (int i = 0; i < M; ++i) {
     int c;
     cin >> c;
@@ -104,12 +72,13 @@ int main() {
       int I, W;
       cin >> I >> W;
       I -= 1;
-      rsum.update_range(range_lo[I], range_hi[I], W);
+      rsum.update(range_lo[I], W);
     } else {
       int I;
       cin >> I;
       I -= 1;
-      cout << rsum.query(range_lo[I], range_lo[I]) << '\n';
+      cout << rsum.range_sum(range_hi[I]) - rsum.range_sum(range_lo[I] - 1)
+           << '\n';
     }
   }
   return 0;
