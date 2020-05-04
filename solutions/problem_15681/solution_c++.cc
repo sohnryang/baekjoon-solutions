36e12fd71ce8a3e88ecb835a17657af482413f6f
손량 <loop.infinitely@gmail.com>
2020-05-04 06:14:09 +0000

    Start to solve problem 15681

---
+++ solutions/problem_15681/solution_c++.cc
@@ -0,0 +1,18 @@
+/*
+ * Baekjoon Online Judge #15681
+ * https://www.acmicpc.net/problem/15681
+ */
+
+#include <ios>
+#include <iostream>
+
+using namespace std;
+
+int N, R, Q;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin >> N >> R >> Q;
+  return 0;
+}
