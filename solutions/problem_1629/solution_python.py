467fcc4977636712e0f6d66034ae8b5e4be62f53
Ryang Sohn <loop.infinitely@gmail.com>
2020-10-23 21:38:09 +0900

    Solve problem 1629 again

--- solutions/problem_1629/solution_python.py
+++ solutions/problem_1629/solution_python.py
@@ -3,12 +3,16 @@ Baekjoon Online Judge #1629
 https://www.acmicpc.net/problem/1629
 """
 
+from sys import stdin
+infast = lambda: stdin.readline().strip()
+
+A, B, C = map(int, infast().split())
 def mod_pow(A, B, C):
     if B == 0:
         return 1
-    elif B % 2 != 0:
-        return A * (mod_pow(A, B // 2, C)) ** 2 % C
-    return (mod_pow(A, B // 2, C)) ** 2 % C
+    if B % 2 == 0:
+        return mod_pow(A, B // 2, C) ** 2 % C
+    else:
+        return A * mod_pow(A, B - 1, C) % C
 
-A, B, C = map(int, input().split())
 print(mod_pow(A, B, C))
