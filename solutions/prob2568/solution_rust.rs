68e21759ad5f26aa1dffaa0e89e1f607b1303edd
Ryang Sohn <loop.infinitely@gmail.com>
2021-12-03 20:24:04 +0900

    Fix a bug in solution

--- solutions/prob2568/solution_rust.rs
+++ solutions/prob2568/solution_rust.rs
@@ -6,7 +6,6 @@
 use std::{
     collections::{HashMap, HashSet},
     io::{stdin, stdout, BufWriter, Write},
-    process::exit,
 };
 
 #[derive(Default)]
@@ -40,32 +39,31 @@ fn main() {
     }
     pairs_input.sort();
     let a_input: Vec<i32> = pairs_input.iter().map(|&x| x.1).collect();
-    if n_input == 1 {
-        println!("0");
-        exit(0);
-    }
 
     let mut d = vec![i32::MAX; n_input + 1];
-    let mut d_idx = vec![-1; n_input + 1];
-    let mut p = HashMap::new();
+    let mut p = vec![0; n_input];
     d[0] = i32::MIN;
     for i in 0..n_input {
         let j = d.partition_point(|&x| x <= a_input[i]);
         if d[j - 1] < a_input[i] && a_input[i] < d[j] {
             d[j] = a_input[i];
-            d_idx[j] = i as i32;
-            *p.entry(a_input[i]).or_default() = d_idx[j - 1]
+            p[i] = j;
         }
     }
 
     let lis_len = d.partition_point(|&x| x < i32::MAX) - 1;
     writeln!(out, "{}", n_input - lis_len).ok();
 
+    let mut len = lis_len as i32;
     let mut seq = vec![];
-    let mut pos: i32 = d_idx[lis_len];
-    while pos != -1 {
-        seq.push(a_input[pos as usize]);
-        pos = p[&a_input[pos as usize]];
+    for (i, &x) in p.iter().enumerate().rev() {
+        if len <= 0 {
+            break;
+        }
+        if x == len as usize {
+            seq.push(a_input[i]);
+            len -= 1;
+        }
     }
     let wires_left: HashSet<i32> = seq.iter().map(|x| b_to_a[x]).collect();
     for wire in pairs_input
