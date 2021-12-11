f70637a4c546b4f5533c2c721960000424dd6bb6
Ryang Sohn <loop.infinitely@gmail.com>
2021-12-11 21:11:32 +0900

    Add escaping to template code

--- templates/template.cc
+++ templates/template.cc
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-int main() {
+int main() {{
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-}
+}}
--- templates/template.rs
+++ templates/template.rs
@@ -3,26 +3,26 @@
  * https://www.acmicpc.net/problem/{prob_no}
  */
 
-use std::io::{stdin, stdout, BufWriter, Write};
+use std::io::{{stdin, stdout, BufWriter, Write}};
 
 #[derive(Default)]
-struct Scanner {
+struct Scanner {{
     buffer: Vec<String>,
-}
-impl Scanner {
-    fn next<T: std::str::FromStr>(&mut self) -> T {
-        loop {
-            if let Some(token) = self.buffer.pop() {
+}}
+impl Scanner {{
+    fn next<T: std::str::FromStr>(&mut self) -> T {{
+        loop {{
+            if let Some(token) = self.buffer.pop() {{
                 return token.parse().ok().expect("Failed parse");
-            }
+            }}
             let mut input = String::new();
             stdin().read_line(&mut input).expect("Failed read");
             self.buffer = input.split_whitespace().rev().map(String::from).collect();
-        }
-    }
-}
+        }}
+    }}
+}}
 
-fn main() {
+fn main() {{
     let mut scan = Scanner::default();
     let mut out = BufWriter::new(stdout());
-}
+}}
