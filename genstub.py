394442765e9c6fb5a512c6a884cfb9c1765011ad
Ryang Sohn <loop.infinitely@gmail.com>
2021-12-28 09:29:25 +0900

    Make genstub.py more usable

--- genstub.py
+++ genstub.py
@@ -1,13 +1,19 @@
 #!/bin/env python
 from sys import argv
-from os import path
+import os
+import re
 
 ext_table = {"py": "python", "cc": "c++", "rs": "rust"}
+regex = re.compile(r"\d+")
 
 if __name__ == "__main__":
     ext = argv[1]
     with open(
-        path.join(path.dirname(__file__), f"templates/template.{ext}")
+        os.path.join(os.path.dirname(__file__), f"templates/template.{ext}")
     ) as f_template:
         with open(f"solution_{ext_table[ext]}.{ext}", "w") as f:
-            f.write(f_template.read().format(prob_no=argv[2]))
+            if len(argv) < 3:
+                prob_no = re.search(regex, os.getcwd())[0]  # type: ignore
+            else:
+                prob_no = int(argv[2])
+            f.write(f_template.read().format(prob_no=prob_no))
