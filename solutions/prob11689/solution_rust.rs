a2de637522c01bee77c541c5f0426c67acf6cf5b
Ryang Sohn <loop.infinitely@gmail.com>
2022-01-02 22:58:56 +0900

    Solve problem #11689

---
+++ solutions/prob11689/solution_rust.rs
@@ -0,0 +1,71 @@
+/*
+ * Baekjoon Online Judge #11689
+ * https://www.acmicpc.net/problem/11689
+ */
+
+use std::{
+    collections::HashMap,
+    io::{stdin, stdout, BufWriter, Write},
+};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let mut out = BufWriter::new(stdout());
+
+    let n_input: usize = scan.next();
+    let bound = (n_input as f64).sqrt() as usize;
+    let mut is_prime = vec![true; bound as usize + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for n in 2..=bound {
+        if !is_prime[n] {
+            continue;
+        }
+        for x in (n.pow(2)..=bound).step_by(n) {
+            is_prime[x] = false;
+        }
+    }
+    let primes = is_prime
+        .iter()
+        .enumerate()
+        .filter(|&(_, &x)| x)
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
+    let mut factorized = HashMap::new();
+    let mut n = n_input;
+    for p in primes {
+        while n % p == 0 {
+            *factorized.entry(p).or_insert(0) += 1;
+            n /= p;
+        }
+        if n == 1 {
+            break;
+        }
+    }
+    if n != 1 {
+        *factorized.entry(n).or_insert(0) += 1;
+    }
+
+    let mut phi = 1;
+    for (p, k) in factorized {
+        phi *= p.pow(k - 1) * (p - 1);
+    }
+    writeln!(out, "{}", phi).ok();
+}
