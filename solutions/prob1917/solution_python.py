fa02c914d0b636ba2854e995b53393f1e430c2b2
Ryang Sohn <loop.infinitely@gmail.com>
2022-01-06 18:57:13 +0900

    Solve problem #1917

---
+++ solutions/prob1917/solution_python.py
@@ -0,0 +1,50 @@
+"""
+Baekjoon Online Judge #1917
+https://www.acmicpc.net/problem/1917
+"""
+
+from sys import stdin
+
+infast = lambda: stdin.readline().strip()
+
+for _ in range(3):
+    grid = []
+    start = (0, 0)
+    for y in range(6):
+        line = [int(x) for x in infast().split()]
+        grid.append(line)
+        if 1 in line:
+            start = (y, line.index(1))
+
+    faces = tuple(range(6))
+    stack = [(start, faces)]
+    cover = dict()
+
+    def avail(y, x):
+        return 0 <= y < 6 and 0 <= x < 6 and grid[y][x] != 0
+
+    dys = [-1, 0, 0, 1]
+    dxs = [0, -1, 1, 0]
+    rot_idx = [
+        (5, 4, 2, 3, 0, 1),
+        (3, 2, 0, 1, 4, 5),
+        (2, 3, 1, 0, 4, 5),
+        (4, 5, 2, 3, 1, 0),
+    ]
+    rot = lambda x, i: tuple([x[j] for j in rot_idx[i]])
+    fail = False
+
+    while stack:
+        p, f = stack.pop()
+        cover[f[0]] = p
+        y, x = p
+        for i, (dy, dx) in enumerate(zip(dys, dxs)):
+            np = (y + dy, x + dx)
+            nf = rot(f, i)
+            if avail(*np) and nf[0] not in cover:
+                stack.append((np, nf))
+
+    if len(cover) != 6:
+        print("no")
+    else:
+        print("yes")
