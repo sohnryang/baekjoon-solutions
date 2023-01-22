d126a05d4c01db2777c0a5090f7470f3ea5ef0f1
Ryang Sohn <loop.infinitely@gmail.com>
2023-01-22 14:16:28 +0900

    Solve problem #1389

---
+++ solutions/prob1389/solution_c++.cc
@@ -0,0 +1,56 @@
+/*
+ * Baekjoon Online Judge #1389
+ * https://www.acmicpc.net/problem/1389
+ */
+
+#include <algorithm>
+#include <ios>
+#include <iostream>
+#include <limits>
+#include <queue>
+#include <set>
+#include <utility>
+
+using namespace std;
+
+const int MAX_N = 100;
+int N, M;
+set<int> graph[MAX_N + 1];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N >> M;
+  for (int i = 0; i < M; i++) {
+    int A, B;
+    cin >> A >> B;
+    graph[A].insert(B);
+    graph[B].insert(A);
+  }
+
+  pair<int, int> res(numeric_limits<int>::max(), 0);
+  for (int src = 1; src <= N; src++) {
+    int dist[MAX_N] = {0};
+    dist[src] = 0;
+    queue<pair<int, int>> q;
+    q.push({0, src});
+    while (!q.empty()) {
+      auto [here_dist, here] = q.front();
+      q.pop();
+      for (auto there : graph[here]) {
+        if (dist[there] != 0)
+          continue;
+        dist[there] = here_dist + 1;
+        q.push({dist[there], there});
+      }
+    }
+    int num = 0;
+    for (int dst = 1; dst <= N; dst++)
+      num += dist[dst];
+    res = min(res, {num, src});
+  }
+  cout << res.second << "\n";
+
+  return 0;
+}
